127db8673bf9cca2b5353629687c9c3a73b16795
Daut Molahasanov <daut.molahasanov@droxic.com>
2019-03-20 10:20:43 +0200

    Add redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { simpleAction } from './actions/simpleAction';
+
 import './App.css';
 import RockPaperScissors from './components/RockPaperScissors'
 
+const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+})
+
+const mapStateToProps = state => ({
+  ...state
+})
+
+
+
 class App extends Component {
+
+  simpleAction = (event) => {
+    this.props.simpleAction();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src="https://cdn.instructables.com/FIU/AIWE/I7Q0TCUT/FIUAIWEI7Q0TCUT.LARGE.jpg" className="App-logo" alt="logo" />
           <RockPaperScissors />
-        </header>
+
+          <div>
+            <button onClick={this.simpleAction}>Test redux action</button>
+
+            <pre>
+            {
+              JSON.stringify(this.props)
+            }
+            </pre>
+          </div>
+
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
